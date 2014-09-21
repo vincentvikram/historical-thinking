22c1bef9f1d01cd861418fb8aa0796f4e1c5503c
Vikram Vincent <vincentvikram@gmail.com>
2014-09-21 21:17:57 +0530

    completed adding most data structures that might be required

--- model/datastructures.h
+++ model/datastructures.h
@@ -19,8 +19,8 @@ struct event {
 };
 
 struct event_connections {
-    event event1;	
-    event event2;
+    struct event event1;
+    struct event event2;
     int order_id; /* priority of the event */
     int impact_factor;	
     string type;
@@ -51,4 +51,63 @@ struct place {
     string type;
     float latitude;
     float longitude;
+};
+
+struct map {
+    int map_id;
+    float latitude;
+    float longitude;
+    int scale;
+    int height;
+    int width;
+};
+
+struct perspective {
+    int event_id;
+    string perspective;
+    struct person person_id;
+    struct group group_id;
+};
+			
+struct event_people {
+    int event_id;
+    struct person person_id;
+    string participation_type;
+};
+
+struct tags {
+    int tag_id;
+    string description;
+    int user_id;
+    int event_id;
+};
+
+struct multimedia {
+    int multimedia_id;
+    string type;
+};
+
+struct event_image{
+    int event_id;
+    int image_id;
+};
+
+struct image_person {
+    int image_id;
+    int person_id;
+};
+
+struct event_place {
+    int event_id;
+    int place_id;
+};
+
+struct event_person {
+    int event_id;
+    int person_id;
+};
+
+struct person_group {
+    int person_id;
+    int group_id;
 };
